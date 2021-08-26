c8d7eda4ae9193f021896b5bb2aebdd2957f9f9c
Germán Andres Ovalle Silva <german.ovalle@ceiba.com.co>
2021-08-26 17:53:46 -0500

    Usando BIConsumer, recibe dos argumentos y devuelve nada, se companara con el metodo normal en java

--- src/main/java/functionalinterface/_Consumer.java
+++ src/main/java/functionalinterface/_Consumer.java
@@ -2,9 +2,11 @@ package functionalinterface;
 
 import imperative.Customer;
 
+import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 
 import static imperative.Customer.greetCustomer;
+import static imperative.Customer.greetCustomerV2;
 
 public class _Consumer {
 
@@ -12,11 +14,20 @@ public class _Consumer {
         // NORMAL JAVA FUNCTION
         Customer customer = new Customer("Maria", "999999");
         greetCustomer(customer);
+        greetCustomerV2(customer, false);
 
         // CONSUMER FUNCTIONAL INTERFACE
         greetCustomerConsumer.accept(customer);
 
+        greetCustomerConsumerV2.accept(customer, false);
+
     }
+
+    static BiConsumer<Customer, Boolean> greetCustomerConsumerV2 =
+            (customer, showPhoneNumber) ->
+            System.out.println("Hello " + customer.customerName + ", thanks for registering " +
+                    (showPhoneNumber ? customer.customerPhoneNumber: "*********"));
+
 
     static Consumer<Customer> greetCustomerConsumer =
             customer -> System.out.println("Hello " + customer.customerName + ", thanks for registering " + customer.customerPhoneNumber);
--- src/main/java/imperative/Customer.java
+++ src/main/java/imperative/Customer.java
@@ -12,4 +12,9 @@ public class Customer {
     public static void greetCustomer(Customer customer){
         System.out.println("Hello " + customer.customerName + ", thanks for registering " + customer.customerPhoneNumber);
     }
+
+    public static void greetCustomerV2(Customer customer, boolean showPhoneNumber){
+        System.out.println("Hello " + customer.customerName + ", thanks for registering " +
+                (showPhoneNumber ? customer.customerPhoneNumber: "*********"));
+    }
 }
