aea249832ebb3782bbd17fe870cb19f7c06d84c9
Germán Andres Ovalle Silva <german.ovalle@ceiba.com.co>
2021-08-26 16:40:16 -0500

    Creamos bloque de codigo funcional que realiza la misma suma con el metodo tradicional

--- src/main/java/functionalinterface/_Function.java
+++ src/main/java/functionalinterface/_Function.java
@@ -1,13 +1,21 @@
 package functionalinterface;
 
+import java.util.function.Function;
+
 public class _Function {
 
     public static void main(String[] args) {
-        int increment = increment(0);
+        int increment = incrementByOne(0);
         System.out.println(increment);
+
+        Integer increment2 = incrementByOneFunction.apply(1);
+        System.out.println(increment2);
     }
 
-    static int increment(int number){
+    static Function<Integer, Integer> incrementByOneFunction =
+            number -> number++;
+
+    static int incrementByOne(int number){
         return number+1;
     }
 
