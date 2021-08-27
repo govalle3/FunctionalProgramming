dd131256a5cddef72595dbcbe33738252ebf5905
Germán Andres Ovalle Silva <german.ovalle@ceiba.com.co>
2021-08-27 09:38:42 -0500

    Uso de ifPresentOrElse, se pueden realizar a partir de la primera funcion, en este caso ofNullable, si tiene valor imprime el correo y si no tiene un mensaje por defecto

--- src/main/java/optionals/Main.java
+++ src/main/java/optionals/Main.java
@@ -6,6 +6,8 @@ public class Main {
     public static void main(String[] args) {
         // logic to do business logic
         Optional.ofNullable(null)
-                .ifPresent(email -> System.out.println("Sending email to " + email));
+                .ifPresentOrElse(
+                        email -> System.out.println("Sending email to " + email),
+                        () -> System.out.println("Cannot send email"));
     }
 }
