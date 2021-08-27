67f368ca3e45cc37bfc0b1f91e98754844c4216c
Germán Andres Ovalle Silva <german.ovalle@ceiba.com.co>
2021-08-27 09:36:28 -0500

    Uso de ifPresent que utiliza un Consumer el cual devuelve nada solo ejecuta una accion que en este caso es imprimir el mail

--- src/main/java/optionals/Main.java
+++ src/main/java/optionals/Main.java
@@ -1,15 +1,11 @@
 package optionals;
 
 import java.util.Optional;
-import java.util.function.Supplier;
 
 public class Main {
     public static void main(String[] args) {
-        Supplier<IllegalStateException> exception_throw =
-                () -> new IllegalStateException("exception throw");
-        
-        Object value = Optional.ofNullable("Hello ")
-                .orElseThrow(exception_throw);
-        System.out.println(value);
+        // logic to do business logic
+        Optional.ofNullable(null)
+                .ifPresent(email -> System.out.println("Sending email to " + email));
     }
 }
