4d76d5fc164f011666f205d119040aea09aab650
Germán Andres Ovalle Silva <german.ovalle@ceiba.com.co>
2021-08-27 09:31:34 -0500

    Extraemos la variable para verificar que .orElseThrow es un supplier aprendido en clases pasadas

--- src/main/java/optionals/Main.java
+++ src/main/java/optionals/Main.java
@@ -1,11 +1,15 @@
 package optionals;
 
 import java.util.Optional;
+import java.util.function.Supplier;
 
 public class Main {
     public static void main(String[] args) {
+        Supplier<IllegalStateException> exception_throw =
+                () -> new IllegalStateException("exception throw");
+        
         Object value = Optional.ofNullable("Hello ")
-                .orElseGet(() -> "default value");
+                .orElseThrow(exception_throw);
         System.out.println(value);
     }
 }
