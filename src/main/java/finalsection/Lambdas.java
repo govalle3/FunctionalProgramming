8f8ad582529c5a55d7933d4ee3993658c4b271f0
Germán Andres Ovalle Silva <german.ovalle@ceiba.com.co>
2021-08-29 21:09:37 -0500

    Uso de la funcion Function y BiFunction para explicar lamdas, nuevamente usamos .apply para ejecutar el metodo

--- src/main/java/finalsection/Lambdas.java
+++ src/main/java/finalsection/Lambdas.java
@@ -1,17 +1,23 @@
 package finalsection;
 
 import java.util.Locale;
+import java.util.function.BiFunction;
 import java.util.function.Function;
 
 public class Lambdas {
     public static void main(String[] args) {
         //Function<String, String> upperCaseName = name -> name.toUpperCase(Locale.ROOT);
         //Function<String, String> upperCaseName = String::toUpperCase;
-        Function<String, String> upperCaseName = name -> {
+        BiFunction<String, Integer,  String> upperCaseName = (name, age) -> {
             // Logic
             if(name.isBlank()) throw new IllegalArgumentException("");
+            System.out.println(age);
             return name.toUpperCase();
         };
+
+        System.out.println(upperCaseName.apply("Alex", 20));
+
+
 
     }
 }
